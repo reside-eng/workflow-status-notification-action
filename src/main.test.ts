16bd799aaa57856799f7dad54e65ef6eced3a45e
mdimajo <mathieu.dimajo@oneup.com>
2021-11-16 19:47:47 -0500

    test: remove username from test + disable tmp codecov

--- src/main.test.ts
+++ src/main.test.ts
@@ -57,7 +57,7 @@ describe('@reside-eng/workflow-status-slack-notification', () => {
         'github-token': `${process.env.GITHUB_TOKEN}`,
       },
       repo: {
-        owner: 'reside-eng',
+        owner: 'repo-owner',
         repo: 'workflow-status-slack-notification',
       },
       workflow: 'Publish Action',
@@ -66,7 +66,7 @@ describe('@reside-eng/workflow-status-slack-notification', () => {
       sha: 'af3ec70',
       ref: 'af3ec704b410630b7fb60b458a4c0aff261959b4',
       eventName: 'pull_request',
-      actor: 'camacho',
+      actor: 'workflowactor',
       serverUrl: 'https://github.com',
     };
 
@@ -138,7 +138,7 @@ describe('@reside-eng/workflow-status-slack-notification', () => {
       'Last run status: completed/failure',
     );
     expect(mockCore.info).toHaveBeenCalledWith(
-      'Message body: {"username":"workflow-status-slack-notification CI alert","icon_emoji":":bangbang:","attachments":[{"color":"good","author_name":"camacho","author_link":"https://github.com/camacho","author_icon":"https://github.com/camacho.png?size=32","fields":[{"title":"Ref","value":"af3ec704b410630b7fb60b458a4c0aff261959b4","short":true},{"title":"Event","value":"pull_request","short":true},{"title":"Action URL","value":"<https://github.com/workflow-status-slack-notification/commit/af3ec70/checks|Publish Action>","short":true},{"title":"Commit","value":"<https://github.com/workflow-status-slack-notification/commit/af3ec70|af3ec70>","short":true},{"title":"Publish Action workflow success","value":"Previously failing Publish Action workflow in workflow-status-slack-notification succeed.","short":false}]}]}',
+      'Message body: {"username":"workflow-status-slack-notification CI alert","icon_emoji":":bangbang:","attachments":[{"color":"good","author_name":"workflowactor","author_link":"https://github.com/workflowactor","author_icon":"https://github.com/workflowactor.png?size=32","fields":[{"title":"Ref","value":"af3ec704b410630b7fb60b458a4c0aff261959b4","short":true},{"title":"Event","value":"pull_request","short":true},{"title":"Action URL","value":"<https://github.com/workflow-status-slack-notification/commit/af3ec70/checks|Publish Action>","short":true},{"title":"Commit","value":"<https://github.com/workflow-status-slack-notification/commit/af3ec70|af3ec70>","short":true},{"title":"Publish Action workflow success","value":"Previously failing Publish Action workflow in workflow-status-slack-notification succeed.","short":false}]}]}',
     );
   });
 
@@ -164,7 +164,7 @@ describe('@reside-eng/workflow-status-slack-notification', () => {
       'Last run status: completed/success',
     );
     expect(mockCore.info).toHaveBeenCalledWith(
-      'Message body: {"username":"workflow-status-slack-notification CI alert","icon_emoji":":bangbang:","attachments":[{"color":"danger","author_name":"camacho","author_link":"https://github.com/camacho","author_icon":"https://github.com/camacho.png?size=32","fields":[{"title":"Ref","value":"af3ec704b410630b7fb60b458a4c0aff261959b4","short":true},{"title":"Event","value":"pull_request","short":true},{"title":"Action URL","value":"<https://github.com/workflow-status-slack-notification/commit/af3ec70/checks|Publish Action>","short":true},{"title":"Commit","value":"<https://github.com/workflow-status-slack-notification/commit/af3ec70|af3ec70>","short":true},{"title":"Publish Action workflow failure","value":"Publish Action workflow in workflow-status-slack-notification failed.","short":false}]}]}',
+      'Message body: {"username":"workflow-status-slack-notification CI alert","icon_emoji":":bangbang:","attachments":[{"color":"danger","author_name":"workflowactor","author_link":"https://github.com/workflowactor","author_icon":"https://github.com/workflowactor.png?size=32","fields":[{"title":"Ref","value":"af3ec704b410630b7fb60b458a4c0aff261959b4","short":true},{"title":"Event","value":"pull_request","short":true},{"title":"Action URL","value":"<https://github.com/workflow-status-slack-notification/commit/af3ec70/checks|Publish Action>","short":true},{"title":"Commit","value":"<https://github.com/workflow-status-slack-notification/commit/af3ec70|af3ec70>","short":true},{"title":"Publish Action workflow failure","value":"Publish Action workflow in workflow-status-slack-notification failed.","short":false}]}]}',
     );
   });
 
