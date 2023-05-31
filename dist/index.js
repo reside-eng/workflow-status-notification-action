76019aa974a128952b1cf135783d62022e7203ba
Scott Prue <prescottprue@users.noreply.github.com>
2023-05-31 13:24:58 -0600

    fix: schedule events marked as reside-camcho [PLAT-4954] (#453)

--- dist/index.js
+++ dist/index.js
@@ -73912,15 +73912,16 @@ async function prepareSlackNotification(message, status) {
     const color = status === 'success' ? 'good' : 'danger';
     const repository = github_1.context.repo.repo;
     const headRef = (0, github_2.getHeadRef)();
+    const authorName = event === 'schedule' ? 'reside-camacho' : `${actor}`;
     const messageBody = {
         icon_emoji: ':bangbang:',
         attachments: [
             {
                 // this defines the attachment block, allows for better layout usage
                 color: `${color}`,
-                author_name: `${actor}`,
-                author_link: `${serverUrl}/${actor}`,
-                author_icon: `${serverUrl}/${actor}.png?size=32`,
+                author_name: authorName,
+                author_link: `${serverUrl}/${authorName}`,
+                author_icon: `${serverUrl}/${authorName}.png?size=32`,
                 fields: [
                     // actual fields
                     {
